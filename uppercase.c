06c20938b478f98455277d6ec9bb98426a099c06
Dheerajb9945 <bdheeraj799@gmail.com>
2025-09-05 17:53:34 +0530

    Add files via upload

---
+++ uppercase.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+char upper,lower;
+printf("\n Enter any character in uppercase:\n ");
+scanf("%c",&upper);
+lower=upper+32;
+printf("\n The character in lower case is: %c",lower);
+}
